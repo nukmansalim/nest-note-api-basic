5013c095669f575ed518ccc4a28e4c854f483720
nukmansalim <nukmansaleem@gmail.com>
2022-10-07 22:23:59 +0700

    setuping product routes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    @Get()
+    findAll(): string {
+        return "this is findAll method"
+    }
+    @Get()
+    findOne(): string {
+        return "this is from findOne method"
+    }
+    @Post()
+    createProduct(): string {
+        return "this is from createProduct method"
+
+    }
+    @Patch()
+    updateProduct(): string {
+        return "this is from updateProduct method"
+    }
+    @Delete()
+    deleteProduct(): string {
+        return "this is from deleteProduct method"
+    }
+}
