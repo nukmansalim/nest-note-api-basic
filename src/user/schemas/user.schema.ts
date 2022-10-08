cc0cf0976af415ea31bb9818d09ceb325757e516
nukmansalim <nukmansaleem@gmail.com>
2022-10-08 17:52:23 +0700

    work on user schema

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document } from "mongoose";
+import { Product } from "../../product/schemas/product.schema";
+export type UserDocument = User & Document
+
+@Schema()
+export class User {
+    @Prop({ required: true })
+    name: string
+
+    @Prop({ required: true })
+    password: string
+
+    @Prop({ required: true })
+    email: string
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "Product" })
+    products: Product[]
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
