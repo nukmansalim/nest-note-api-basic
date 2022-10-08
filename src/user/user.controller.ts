eb0dc157fdaf6fc8526c2c1832c89ea9a3155c7c
nukmansalim <nukmansaleem@gmail.com>
2022-10-08 19:01:30 +0700

    working on user service and controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) { }
+    @Get()
+    getUsers() {
+        return
+    }
+    @Get()
+    getSingleUser() {
+        return
+    }
+    @Post()
+    createUser() {
+        return
+    }
+    @Patch()
+    updateUser() {
+        return
+    }
+    @Delete()
+    deleteUser() {
+        return
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from "bcrypt"
+import { User, UserDocument } from './schemas/user.schema';
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel(User.name)
+    private userModel: Model<UserDocument>) { }
+
+    async findAllUsers() {
+        return this.userModel.find().select("-password").exec()
+    }
+
+    async findSingleUser() {
+        return this.userModel.findOne().select("-password").exec()
+    }
+
+    async createUser() {
+
+    }
+}
