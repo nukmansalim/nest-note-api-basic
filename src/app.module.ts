812f2fdac28cc175275eb06a23e0172d84e3c800
nukmansalim <nukmansaleem@gmail.com>
2022-10-08 17:44:30 +0700

    making user folder and file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost:27017/nest-api-basic')],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost:27017/nest-api-basic'), UserModule],
 
 })
 export class AppModule { }
