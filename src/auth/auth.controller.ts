34078e306636496b75eb0264fae92cc96bd7d3c8
nukmansalim <nukmansaleem@gmail.com>
2022-10-09 01:37:56 +0700

    implementing passport local

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Body, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { loginDto } from './dto/login.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+    @Post()
+    @UseGuards(LocalAuthGuard)
+    validateUser(@Body() dto: loginDto) {
+        return this.authService.validateUser(dto.email, dto.password)
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from '../user/schemas/user.schema';
+import * as bcrypt from "bcrypt"
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectModel(User.name)
+    private userModel: Model<UserDocument>) { }
+    async validateUser(email: string, pass: string) {
+        const user = await this.userModel.findOne({ email })
+        if (user && bcrypt.compare(pass, user.password)) {
+            const { password, ...result } = user
+            return result
+        }
+        return null
+    }
+}
